80f5681f69ea0cf8ab909bf5099dd8581dee3439
AlapatiSreeHarsha <alapatisreeharsha139@gmail.com>
2026-07-06 23:52:17 +0530

    LSP

---
+++ LSP.java
@@ -0,0 +1,30 @@
+//A child class should be able to replace its parent class without causing problems.
+//Wherever you use a parent object, you should also be able to use any child object.
+class Notification{
+    public void sendNotification(){
+        System.out.println("Sending notification");
+    }
+}
+
+class EmailNotification extends Notification{
+    public void sendNotification(){
+        System.out.println("Sending email notification");
+    }
+}
+
+class SMSNotification extends Notification{
+    public void sendNotification(){
+        System.out.println("Sending SMS notification");
+    }
+}
+
+
+public class LSP {
+    public static void main(String[] args) {
+        Notification notification1=new EmailNotification();
+        notification1.sendNotification();
+
+        Notification notification2=new SMSNotification();
+        notification2.sendNotification();
+    }
+}
