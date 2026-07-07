cda2e0325380461a6bc768c5ff264e690aeaeb5f
AlapatiSreeHarsha <alapatisreeharsha139@gmail.com>
2026-07-07 11:06:56 +0530

    ISP

---
+++ ISP.java
@@ -0,0 +1,64 @@
+//Create small, specific interfaces instead of one big interface.
+
+interface Rider{
+    void bookRide();
+    void cancelRide();
+    void trackRide();
+    void payForRide();
+}
+
+interface Driver{
+    void acceptRide();
+    void startRide();
+    void endRide();
+    void updateLocation();
+}
+
+class RiderApp implements Rider{
+    public void bookRide(){
+        System.out.println("Booking ride");
+    }
+
+    public void cancelRide(){
+        System.out.println("Cancelling ride");
+    }
+
+    public void trackRide(){
+        System.out.println("Tracking ride");
+    }
+
+    public void payForRide(){
+        System.out.println("Paying for ride");
+    }
+}
+
+class DriverApp implements Driver{
+    public void acceptRide(){
+        System.out.println("Ride accepted");
+    }
+
+    public void startRide(){
+        System.out.println("Ride started");
+    }
+
+    public void endRide(){
+        System.out.println("Ride ended");
+    }
+
+    public void updateLocation(){
+        System.out.println("Location updated");
+    }
+}
+
+public class ISP{
+    public static void main(String[] args){
+        Rider rider=new RiderApp();
+        Driver driver=new DriverApp();
+
+        rider.bookRide();
+        rider.trackRide();
+
+        driver.acceptRide();
+        driver.startRide();
+    }
+}
